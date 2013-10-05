af852218964be9767e522bc02c138b9005f98c89
Nano Molina <nanomolinacav@gmail.com>
2013-10-05 16:41:38 -0300

    insert data

---
+++ insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO `Libro`(`ISBN`, `Titulo`, `editorial`, `edicion`)
+VALUES ("1", "aa", "bb", "4"); 
+
+INSERT INTO `Libro`
+VALUES ("8761", 
+	"Sistemas operativos modernos", 
+	"Pearson Educación", 
+	"2");
+
+INSERT INTO `Libro`
+VALUES ("7369",
+    	"Computer Organization and Design",
+        "Elsevier.inc",
+        "4");
