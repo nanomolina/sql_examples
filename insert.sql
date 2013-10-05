6ac5937e5a76625b8d0be5e933a622b40b4a4e61
Nano Molina <nanomolinacav@gmail.com>
2013-10-05 19:41:39 -0300

    insert tuples

--- insert.sql
+++ insert.sql
@@ -9,14 +9,14 @@ INSERT INTO `Persona` (`apellido`, `rest_nom`, `DNI`) VALUES
 ('Verblud', 'David Javier', 37696497);
 
 INSERT INTO `Libro` VALUES 
-("8761", "Sistemas operativos modernos", "Pearson Educación", "2"),
-("7369", "Computer Organization and Design", "Elsevier.inc", "4"),
-("9364", "Redes de computadoras", "Prentice Hall", "4"),
-("6492", "Fundamentos de bases de datos", "McGraw Hill", "3");
+(8761, 'Sistemas operativos modernos', 'Pearson Educación', '2'),
+(7369, 'Computer Organization and Design', 'Elsevier.inc', '4'),
+(9364, 'Redes de computadoras', 'Prentice Hall', '4'),
+(6492, 'Fundamentos de bases de datos', 'McGraw Hill', '3');
 
 INSERT INTO `Libro_autor`(`ISBN`, `nomb_autor`) VALUES
-("1345", "Jay L. Devore"),
-("7369", "David A. Patterson"),
-("8761", "Andrew S. Tanenbaum"),
-("9364", "Andrew S. Tanenbaum"),
-("6492", "Avi Silberschatz");
+(1345, 'Jay L. Devore'),
+(7369, 'David A. Patterson'),
+(8761, 'Andrew S. Tanenbaum'),
+(9364, 'Andrew S. Tanenbaum'),
+(6492, 'Avi Silberschatz');
